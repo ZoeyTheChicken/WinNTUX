e1e243d5942b0fd361f977aecd5e6959f1ab51ba
Zoey.Dev <110342458+ZoeyTheChicken@users.noreply.github.com>
2024-07-15 13:08:58 -0400

    Upload Source code for DLL ver. 1.8.0

---
+++ WinNTUX/WallpaperHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WinNTUX
+{
+    public class WallpaperHandler
+    {
+        private const uint SPI_SETDESKWALLPAPER = 0x0014;
+        private const uint SPIF_UPDATEINIFILE = 0x01;
+        private const uint SPIF_SENDCHANGE = 0x02;
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SystemParametersInfo(uint uiAction, uint uiParam, string pvParam, uint fWinIni);
+
+        /// <summary>
+        /// Changes the desktop wallpaper.
+        /// </summary>
+        /// <param name="imagePath">The path to the image file.</param>
+        /// <returns>True if the wallpaper was successfully changed, otherwise false.</returns>
+        public bool SetDesktopWallpaper(string imagePath)
+        {
+            int result = SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, imagePath, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+            return result != 0;
+        }
+    }
+}
